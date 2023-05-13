2ea12ab63c02a718470181fefb0b3b37ffbc26a3
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2023-05-13 13:38:11 +0200

    Add database

--- dist/index.js
+++ dist/index.js
@@ -4,10 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const api_1 = __importDefault(require("./routes/api"));
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+const database = process.env.MONGOLAB_URI;
+mongoose.connect(database, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => console.log('e don connect'))
+    .catch(err => console.log(err));
 const PORT = process.env.PORT || 5000;
 app.use('/', express.static(path.join(__dirname, '../views'), { extensions: ['html'] }));
 app.use('/api', api_1.default);
-app.listen(PORT, console.log("Listening on port: " + PORT));
+app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import { Request, Response } from "express";
 import apiRouter from './routes/api';
 
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const database = process.env.MONGOLAB_URI;
+mongoose.connect(database, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+    .then(() => console.log('e don connect'))
+    .catch((err: any) => console.log(err)); // Fix error type
+
 const PORT = process.env.PORT || 5000;
 
 app.use('/', express.static(path.join(__dirname, '../views'), { extensions: ['html'] } ));
-
 app.use('/api', apiRouter);
-
-app.listen(PORT, console.log("Listening on port: " + PORT));
+app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
