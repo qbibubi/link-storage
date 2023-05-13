bfa5738679e2eb2f7168759d68af8f5718c6bdd1
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2023-05-13 13:37:48 +0200

    Spacing

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,17 +7,21 @@ router.use((req: Request, res: Response, next: NextFunction) => {
   console.log('Time: ', Date.now())
   next()
 })
+
 router.get('/links', (req: Request, res: Response) => {
   res.json({
     "id": 1,
   });
 })
+
 router.patch('/links', (req: Request, res: Response) => {
   res.send('')
 })
+
 router.post('/links', (req: Request, res: Response) => {
   res.send('')
 })
+
 router.delete('/links', (req: Request, res: Response) => {
   res.send('')
 })
